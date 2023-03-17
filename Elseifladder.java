41961d5fdf660f300569e1602b509fc0bead9d95
Pbanso1919 <127472899+Pbanso1919@users.noreply.github.com>
2023-03-17 12:22:55 +0530

    Add files via upload

---
+++ Elseifladder.java
@@ -0,0 +1,15 @@
+class Elseifladder{
+	public static void main(String args[]){
+		int time = 20;
+		if (time < 18){
+			System.out.println("good day");
+		}
+		else if(time == 16){
+			System.out.println("good evening");
+		}
+		else{
+			System.out.println("good well day");
+		}
+
+	}
+}
