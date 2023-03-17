1213256b074cbae6403122cc88c4d24481cb4f3b
Pbanso1919 <127472899+Pbanso1919@users.noreply.github.com>
2023-03-17 12:47:02 +0530

    Add files via upload

---
+++ Nestedif.java
@@ -0,0 +1,19 @@
+class Nestedif{
+	public static void main(String args[]){
+		String address = "kolkata, India";
+		if(address.endwith("India")){
+			if(address.contains("Delhi")){
+				System.out.println("Your city is Delhi");
+			}
+			else if(address.contains("Mumbai")){
+				System.out.println("Your city is Mumbai");
+			}
+			else{
+				System.out.println("address.spilt(",")[0];
+
+			}
+			else{
+				System.out.printl("You are not living in India");
+			}
+		}
+	}
